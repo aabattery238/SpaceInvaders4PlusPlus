65a4730589bea5f43dddab2364afb918f33d56fb
subtr0pical <derektung1925@gmail.com>
2025-05-28 13:36:14 -0400

    Update FinalCulminating.py

--- FinalCulminating.py
+++ FinalCulminating.py
@@ -1,5 +1,5 @@
 from pygame import *
-
+from pygame.locals import *
 class bullet:
     def __init__(movementSpeed, angle):
         self.movementSpeed = movementSpeed
@@ -20,3 +20,32 @@ while running:
             running = False
         if event.type == KEYDOWN:
             draw.circle()
+
+#starts here
+pygame.init()
+screen = pygame.display.set_mode((800,800))
+color = [(255,0,0),(0,255,0)]
+pygame.display.set_caption('im pygaming it')
+clock = pygame.time.Clock()
+running = True
+x = 300
+y = 300
+speed = 10
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    keys = pygame.key.get_pressed()
+    if keys[K_d]:
+        x+=speed
+    if keys[K_a]:
+        x-=speed
+    if keys[K_w]:
+        y-=speed
+    if keys[K_s]:
+        y+=speed
+    clock.tick(60)
+    screen.fill((0, 0, 0)) 
+    fish = pygame.draw.circle(screen,(255,0,255),(x,y),(10),5)
+    pygame.display.update()
+pygame.quit()   
