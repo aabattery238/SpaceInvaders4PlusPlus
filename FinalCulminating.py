e8a05a599946f20837413eb6aa2dad30151d127f
Abdullah Atif <abdullahatifsaeed@icloud.com>
2025-05-30 13:24:53 -0400

    Fixed Mysterious bug

--- FinalCulminating.py
+++ FinalCulminating.py
@@ -9,45 +9,45 @@ bulletRadius = 5
 enemyRadius = 10
 
 class bullet:
-    def __init__(self, position, direction, speed, existance):
+    def __init__(self, position, direction, speed, existance, colour):
         self.position = position
         self.direction = direction
         self.speed = speed
         self.existance = existance
-        self.rect = pygame.Rect(self.position[0], self.position[1], bulletRadius*2, bulletRadius*2)
+        self.colour = colour
+        self.visual = pygame.draw.circle(mainscreen, self.colour, self.position, 5)
 
     def update(self):
         self.position[0] += self.direction[0] * self.speed
         self.position[1] += self.direction[1] * self.speed
-        self.rect.topleft = (self.position[0], self.position[1])
+        self.visual = pygame.draw.circle(mainscreen, self.colour, self.position, 5)
     
     def draw(self, screen):
-        pygame.draw.circle(screen, (255, 255, 255), (int(self.position[0]), int(self.position[1])), bulletRadius)
+        self.visual = pygame.draw.circle(screen, self.colour, self.position, 5)
 
 class enemy:
     def __init__(self, position, existance):
         self.position = position
         self.existance = existance
-        self.rect = pygame.Rect(self.position[0] - enemyRadius, self.position[1] - enemyRadius, enemyRadius*2, enemyRadius*2)
 
     def draw(self, surface):
-        pygame.draw.circle(surface, (255, 0, 0), self.position, enemyRadius)
+        self.visual = pygame.draw.circle(surface, (255, 0, 0), self.position, enemyRadius)
 
 pygame.init()
-playerPosition = [300, 300]
+characterAsset = pygame.image.load("playersprite1.png")
+characterRect = characterAsset.get_rect(center=(400, 400))
+
 bulletCooldownLength = 1
 bulletCooldown = 0
 playerSpeed = 5
 defaultBulletSpeed = 10
 clock = pygame.time.Clock()
 mainscreen = pygame.display.set_mode((800,800))
-existingBullets = [bullet(playerPosition, [0, 0], defaultBulletSpeed, False)]
+existingBullets = [bullet([characterRect.x, characterRect.y], [0, 0], defaultBulletSpeed, False, (255, 255, 255))]
 existingEnemies = [enemy((400, 400), True)]
 running = True
 
-#Assets
-characterAsset = pygame.image.load("playersprite1.png")
-characterRect = characterAsset.get_rect(center=(400, 400))
+
 
 while running:
     clock.tick(60)
@@ -59,34 +59,37 @@ while running:
     
     keys = pygame.key.get_pressed()
 
+    if keys[K_d]:
+        characterRect.x += playerSpeed
     if keys[K_a]:
-        playerPosition[0] += playerSpeed
-    if keys[K_a]:
-        playerPosition[0] -= playerSpeed
-    if keys[K_a]:
-        playerPosition[1] -= playerSpeed
-    if keys[K_a]:
-        playerPosition[1] += playerSpeed
+        characterRect.x -= playerSpeed
+    if keys[K_w]:
+        characterRect.y -= playerSpeed
+    if keys[K_s]:
+        characterRect.y += playerSpeed
 
     if keys[K_SPACE] and bulletCooldown <= 0:
+        print("cooldown")
         mouseX, mouseY = pygame.mouse.get_pos()
-        dx, dy = mouseX - playerPosition[0], mouseY - playerPosition[1]
+        dx, dy = mouseX - characterRect.x, mouseY - characterRect.y
         distance = hypot(dx, dy)
         
         if distance != 0:
+            print("distance")
             direction = [dx / distance, dy / distance]
-            existingBullets.append(bullet(playerPosition, direction, defaultBulletSpeed, True))
+            existingBullets.append(bullet([characterRect.x, characterRect.y], direction, defaultBulletSpeed, True, (255, 255, 255)))
             bulletCooldown = bulletCooldownLength
 
     for bullets in existingBullets:
-        bullets.update()
-        bullets.draw(mainscreen)
+        if bullets.existance:
+            bullets.update()
+            bullets.draw(mainscreen)
 
     for enemies in existingEnemies:
         enemy.draw(enemies, mainscreen)
         
         for bullets in existingBullets:
-            if enemies.rect.colliderect(bullets.rect) and enemies.existance and bullets.existance:
+            if (enemies.visual).colliderect(bullets.visual) and enemies.existance and bullets.existance:
                 bullets.existance = False
                 enemies.existance = False
                 existingBullets.remove(bullets)
@@ -94,10 +97,11 @@ while running:
                 newPos = (random.randint(0, 800), random.randint(0, 800))
                 existingEnemies.append(enemy(newPos, True))
                 break
-            
+
     bulletCooldown -= 0.1
 
-    characterRect.center = [1, 1]
+    
+    characterRect.clamp_ip(mainscreen.get_rect())
     mainscreen.blit(characterAsset, characterRect)
 
     pygame.display.flip()
